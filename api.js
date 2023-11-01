da4d096485209ba0fc00a64ad28cac46919271c9
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-01 22:13:39 +0800

    update

--- api.js
+++ api.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const moment = require('moment');
+// api.js
 
-const app = express();
+const moment = require('moment');
 
 const apiUrl = 'https://g3rvbpemgm.us.aircode.run/counter';
 
-app.get('/api', async (req, res) => {
+module.exports = async (req, res) => {
     moment.locale("zh-cn");
 
     const param = new URLSearchParams(req.url.split("?")[1]);
     const name = param.get("name");
     var font = param.get("font");
-    if(!font) font = 16;
+    if (!font) font = 16;
 
     const { default: fetch } = await import('node-fetch');
 
@@ -27,16 +26,16 @@ app.get('/api', async (req, res) => {
 
     res.setHeader("Content-Type", "image/svg+xml");
     res.send(`
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 180">
-        <style>
-            .text {
-                font-family: Microsoft YaHei;
-                font-size: ${font}px;
-            }
-        </style>
-        <g id="detail">
-            <text class="text">${counterView}</text>
-        </g>
-    </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 180">
+            <style>
+                .text {
+                    font-family: Microsoft YaHei;
+                    font-size: ${font}px;
+                }
+            </style>
+            <g id="detail">
+                <text class="text">${counterView}</text>
+            </g>
+        </svg>
     `);
-});
+};
