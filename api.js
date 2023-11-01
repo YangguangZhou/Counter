e49d5384c76046e3f2b673e5481954cfaf56e3ff
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-01 21:11:46 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,29 +1,30 @@
 const apiUrl = 'https://g3rvbpemgm.us.aircode.run/counter';
-var counterView;
+var param;
 
-(async () => {
-  const result = await (await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({token: location.href}),
-  })).json();
-  counterView=result.times;
-})();
+async function getCount(){
+    const result = await(await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: param.get("name") }),
+    })).json();
+    return result.times;
+};
 
 module.exports = async (req, res) => {
     moment.locale("zh-cn");
     param = new URLSearchParams(req.url.split("/api")[1]);
+    console.log(param.get("name"));
     res.setHeader("Content-Type", "image/svg+xml");
     const {
-        count = counterView,
+        counterView = getCount(),
     } = req.query;
 
     res.send(`
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 180">
     <g id="detail">
-        <div id="app">这是第 <strong>${count}</strong> 次访问</div>
+        <div id="app">这是第 <strong>${counterView}</strong> 次访问</div>
     </g>
     </svg>`);
 };
