18815519395eeda358e80cae5a7afaee8c77148c
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-01 21:27:55 +0800

    update

--- api.js
+++ api.js
@@ -1,25 +1,27 @@
 const apiUrl = 'https://g3rvbpemgm.us.aircode.run/counter';
-var param;
 
-async function getCount(){
+async function getCount(name){
     const result = await(await fetch(apiUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name: param.get("name") }),
+        body: JSON.stringify({ name }),
     })).json();
     return result.times;
 };
 
 module.exports = async (req, res) => {
+    const moment = require("moment"); // 引入 moment 库
     moment.locale("zh-cn");
-    param = new URLSearchParams(req.url.split("/api")[1]);
-    console.log(param.get("name"));
+
+    const param = new URLSearchParams(req.url.split("/api")[1]);
+    const name = param.get("name");
+    console.log(name);
+
     res.setHeader("Content-Type", "image/svg+xml");
-    const {
-        counterView = getCount(),
-    } = req.query;
+
+    const counterView = await getCount(name); // 调用 getCount 并传递 name 参数
 
     res.send(`
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 180">
