41709cb79e0a8562f26175c904baed694a2c3539
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-01 21:41:26 +0800

    update

--- api.js
+++ api.js
@@ -1,6 +1,18 @@
+const express = require('express');
+const moment = require('moment');
+
+const app = express();
+
 const apiUrl = 'https://g3rvbpemgm.us.aircode.run/counter';
 
-async function getCount(name){
+app.get('/api', async (req, res) => {
+    moment.locale("zh-cn");
+
+    const param = new URLSearchParams(req.url.split("?")[1]);
+    const name = param.get("name");
+
+    const { default: fetch } = await import('node-fetch');
+
     const result = await(await fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -8,25 +20,14 @@ async function getCount(name){
         },
         body: JSON.stringify({ name }),
     })).json();
-    return result.times;
-};
 
-module.exports = async (req, res) => {
-    const moment = require("moment"); // 引入 moment 库
-    moment.locale("zh-cn");
-
-    const param = new URLSearchParams(req.url.split("/api")[1]);
-    const name = param.get("name");
-    console.log(name);
+    const counterView = result.times;
 
     res.setHeader("Content-Type", "image/svg+xml");
-
-    const counterView = await getCount(name); // 调用 getCount 并传递 name 参数
-
     res.send(`
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 180">
     <g id="detail">
         <div id="app">这是第 <strong>${counterView}</strong> 次访问</div>
     </g>
     </svg>`);
-};
+});
