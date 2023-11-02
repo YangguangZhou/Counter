6e42b804b14e874a7b8bff793600c856268950be
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-02 22:37:58 +0800

    Update api.js

--- api/api.js
+++ api/api.js
@@ -3,14 +3,11 @@ const apiUrl = 'https://g3rvbpemgm.us.aircode.run/counter';
 
 module.exports = async (req, res) => {
     moment.locale("zh-cn");
-
     const param = new URLSearchParams(req.url.split("?")[1]);
     const name = param.get("name");
     var font = param.get("font");
     if (!font) font = 16;
-
     const { default: fetch } = await import('node-fetch');
-
     const result = await (await fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -18,9 +15,7 @@ module.exports = async (req, res) => {
         },
         body: JSON.stringify({ name }),
     })).json();
-
     const counterView = result.times;
-
     res.setHeader("Content-Type", "image/svg+xml");
     res.send(`
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 180">
