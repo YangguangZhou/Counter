95b0baa6e6a1bc88db0eb1761b22982e74bbd2de
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-03 23:56:21 +0800

    up

--- api/api.js
+++ api/api.js
@@ -1,20 +1,6 @@
 const moment = require('moment');
 const apiUrl = 'https://g3rvbpemgm.us.aircode.run/counter';
 
-const { createCanvas } = require('canvas');
-
-function getTextDimensions(text, font) {
-    const canvas = createCanvas(200, 200);
-    const context = canvas.getContext('2d');
-    context.font = font;
-    const metrics = context.measureText(text);
-    return {
-        width: metrics.width,
-        // The height calculation is a workaround because the height is not directly given by the measureText method
-        height: Math.ceil(parseFloat(getComputedStyle(context.canvas).getPropertyValue('font-size')))
-    };
-}
-
 module.exports = async (req, res) => {
     moment.locale("zh-cn");
     const param = new URLSearchParams(req.url.split("?")[1]);
@@ -34,14 +20,9 @@ module.exports = async (req, res) => {
     })).json();
     const counterView = result.times;
     var color = (dark == 1) ? "#fff" : "#000";
-
     // 计算文本的宽度和高度
-    // const textWidth = counterView.toString().length * font; // 根据字数和字体大小计算宽度
-    // const textHeight = font; // 字体大小即为文本的高度
-    const fonts = 'regular ' + font + 'px Microsoft YaHei';
-    const text = getTextDimensions(counterView.toString(), fonts);
-    const textWidth = text.width;
-    const textHeight = text.height;
+    const textWidth = counterView.toString().length * font; // 根据字数和字体大小计算宽度
+    const textHeight = font; // 字体大小即为文本的高度
     res.setHeader("Content-Type", "image/svg+xml");
     res.send(`
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${textWidth}" height="${textHeight}">
