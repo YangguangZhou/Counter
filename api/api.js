ac5e10478a7215e8c06114a04dcd677ddf94949a
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-04 12:29:52 +0800

    Update api.js

--- api/api.js
+++ api/api.js
@@ -21,7 +21,7 @@ module.exports = async (req, res) => {
     const counterView = result.times;
     var color = (dark == 1) ? "#fff" : "#000";
     // 计算文本的宽度和高度
-    const textWidth = counterView.toString().length * font; // 根据字数和字体大小计算宽度
+    const textWidth = counterView.toString().length * font / 2 + 2; // 根据字数和字体大小计算宽度
     const textHeight = font; // 字体大小即为文本的高度
     res.setHeader("Content-Type", "image/svg+xml");
     res.send(`
