c0e3b937ee7c0941da24cc3e49087b9905aeb818
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-05 17:41:47 +0800

    update

--- api/view.js
+++ api/view.js
@@ -9,8 +9,9 @@ module.exports = async (req, res) => {
     var font = param.get("font");
     var color = param.get("color");
     if (!name) name = "counter";
-    if ((!dark)&&(!color)) color = "#000";
-    else if((!color)&&dark) color = "#fff";
+    if ((!dark) && (!color)) color = "#000";
+    else if ((!color) && dark) color = "#fff";
+    if(color[0] != "#"&&color[0] != "r") color = "#" + color;
     if (!font) font = 16;
     const { default: fetch } = await import('node-fetch');
     const result = await (await fetch(apiUrl, {
@@ -22,7 +23,7 @@ module.exports = async (req, res) => {
     })).json();
     const counterView = result.times;
     // 计算文本的宽度和高度
-    const textWidth = Math.ceil(counterView.toString().length * font / 2.0 + counterView.toString().length*1.5); // 根据字数和字体大小计算宽度
+    const textWidth = Math.ceil(counterView.toString().length * font / 2.0 + counterView.toString().length * 1.5); // 根据字数和字体大小计算宽度
     const textHeight = font; // 字体大小即为文本的高度
     res.setHeader("Content-Type", "image/svg+xml");
     res.send(`
